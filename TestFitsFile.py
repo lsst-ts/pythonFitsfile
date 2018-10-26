a5a2582b60703170ddf22ea7567d592398cac6cd
Andres Anania <aanania@lsst.org>
2018-10-26 20:20:13 -0300

    Initial commit

---
+++ TestFitsFile.py
@@ -0,0 +1,21 @@
+import unittest
+import PythonFits
+import numpy as np
+
+class TestFitsFile(unittest.TestCase):
+
+    def setUp(self):
+        self.fitsFile = PythonFits.PythonFits("C:\\Users\\aanania\\Documents\\ts_python_fitsfile", "testFits", separator='\\')
+
+    def test_addDataAndCloseFile(self):
+        data = np.arange(100.0)
+        self.fitsFile.addData(data)
+        self.fitsFile.closeFile()
+
+    def test_testChecksum(self):
+        a = len(self.fitsFile.getChecksum())
+        self.assertGreater(a, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
